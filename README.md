a310e14560c37314d9a9d88aad9c25bfda7df359
codui <emailtowork1@proton.me>
2024-07-23 17:02:28 +0300

    Created a remote repository and added all files to it

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gulp build created based on ITDoctor lessons
+# Source https://youtube.com/playlist?list=PLuY6eeDuleIM-cQvHBy4jQnGH0fVZ_RhK&si=ZP2qz94NHXBrj-Fy
---
+++ gulpfile.js
@@ -0,0 +1,161 @@
+// const gulp = require('gulp');
+// const rename = require('gulp-rename');
+// const cleanCSS = require('gulp-clean-css');
+
+// const sass = require('gulp-sass')(require('sass'));
+
+// const paths = {
+//     styles: {
+//         src: 'src/styles/**/*.scss', // источник откуда будут браться исходные файлы
+//         dest: 'dist/css/' // конечная папка с готовыми файлами
+//     },
+//     scripts: {
+//         src: 'src/styles/**/*.js', // источник где будут исходные файлы
+//         dest: 'dist/js/' // конечная папка с готовыми файлами
+//     }
+// }
+
+// async function clean() { // Cleaning folders
+//     const { deleteAsync } = await import('del');
+//     return deleteAsync(['dist']) // Указываем папку, которая будет очищаться
+// }
+
+// function styles() {
+//     return gulp.src(paths.styles.src)// Передаём путь откуда берутся файлы для обработки
+//     // Подключаем необходимые модули для обработки переданных файлов
+//         .pipe(sass().on('error', sass.logError)) // Компеляция SCSS в CSS
+//         .pipe(cleanCSS()) // Минификация файлов CSS - удаление пробелов, лишних ";", всех абзацов
+//         .pipe(rename({
+//             basename: 'main',
+//             suffix: '.min'
+//         }))
+//         .pipe(gulp.dest(paths.styles.dest));
+// }
+
+
+// function watch() { // Отслеживаем изменения
+//     gulp.watch(paths.styles.src, styles) // Сперва указываем путь к файлам которые будем отслеживать, потом передаётся таска (функция) которая будет выполняться при изменении в этих файлах
+
+// }
+// exports.clean = clean; // "cleaner" - name of command in gulp, "clean" - name of our function
+// exports.styles = styles;
+// exports.watch = watch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+// ! const sourcemaps = require('gulp-sourcemaps')
+// ! const autoprefixer = require('autoprefixer');
+
+// Paths to files
+const paths = {
+    styles: {
+        src: 'src/styles/**/*.scss', // источник откуда будут браться исходные файлы
+        dest: 'dist/css/' // конечная папка с готовыми файлами
+    },
+    scripts: {
+        src: 'src/scripts/**/*.js', // источник где будут исходные файлы
+        dest: 'dist/js/' // конечная папка с готовыми файлами
+    }
+};
+
+async function clean() { // Cleaning folders
+    const { deleteAsync } = await import('del');
+    return deleteAsync(['dist']); // Указываем папку, которая будет очищаться
+}
+
+// Processing style files. Compilation of SCSS into CSS and other operations
+function styles() {
+    return gulp.src(paths.styles.src // Передаём путь откуда берутся файлы для обработки
+        //     , {
+        //     sourcemaps: true
+        // }
+        )
+        // ! .pipe(sourcemaps.init())
+        .pipe(sass(
+            //     {
+            //     outputStyle: 'expanded',
+            //     indentWidth: 4,
+            // }
+        ).on('error', sass.logError)) // Компиляция SCSS в CSS
+        // .pipe(postcss([autoprefixer({
+        //     cascade: false
+        // })]))
+        // ! .pipe(sourcemaps.write())
+        .pipe(cleanCSS()) // Минификация файлов CSS - удаление пробелов, лишних ";", всех абзацов
+        .pipe(rename({
+            basename: 'main',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+// Processing javascript files
+function scripts() {
+    return gulp.src(paths.scripts.src, {
+        sourcemaps: true
+    })
+        .pipe(babel()) // Транспилирует код в код старого стандарта, для старых браузеров
+        .pipe(uglify()) // Минифицирует, сжимает и оптимизирует код
+        .pipe(concat('main.min.js')) // Объединяем файлы в один и сразу даём название объединённому файлу
+        .pipe(gulp.dest(paths.scripts.dest))
+}
+
+function watch() { // Отслеживаем изменения
+    gulp.watch(paths.styles.src, styles); // Сперва указываем путь к файлам которые будем отслеживать, потом передаётся таска (функция) которая будет выполняться при изменении в этих файлах
+    gulp.watch(paths.scripts.src, scripts);
+}
+
+
+
+// Экспортируем задачи
+exports.clean = clean; // "cleaner" - name of command in gulp, "clean" - name of our function
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watch = watch;
+
+// series() выполняет задачи последовательно
+const buildSeris = gulp.series(clean, gulp.parallel(styles, scripts), watch);
+// const buildParalel = gulp.parallel(clean, styles); // parallel() выполняет задачи паралельно
+
+exports.build = buildSeris; // To run task write in terminal 'gulp build'
+exports.default = buildSeris; // Just write in terminal 'gulp'
